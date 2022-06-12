af0fd1d0f520997140d74772c77fe95b14b6721b
niranjankl <niranjan.kl@gmail.com>
2022-06-12 21:23:24 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Hello World Demo...12 June 2022 git webhook trigger \n")
+print("Hello World Demo...12 June 2022 git webhook trigger finally \n")
 
 
 
